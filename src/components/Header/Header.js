d49bde34e90fa82a0436e0e1247c0e86e09039bb
marcosoliveira <marcosroliveira@outlook.com>
2020-06-11 21:56:54 -0300

    funcao no logo voltar para home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import ninja from './ImgNinja.png'
+import { LayoutFull } from '../LayoutFullContainer/LayoutFullContainer'
 
 const HeaderContainer = styled.div`
   background: lightblue;
     display: flex;
     justify-content: flex-start;
     align-items: center;
-    height: 5vh;
+    height: 15vh;
     padding: 0 10px;
 `
+
+const Image = styled.img`
+  width: 7vw;
+  height: 13vh;
+`
+
 export class Header extends Component {
   render() {
     return (
       <HeaderContainer>
-          Headers
+          <a href="/"><Image src={ninja} alt="Logo"/></a>
       </HeaderContainer>
     )
   }
